968d470dbe68f88056f356164779b13c0896103b
goosegoesquack <hongyuan22222@gmail.com>
2025-11-01 17:38:01 +0000

    reorganising into folders

--- index.js
+++ index.js
@@ -42,12 +42,6 @@ app.get("/login", (req, res) => {
 app.get("/", (req, res) => {
     res.sendFile("index.html", {root: __dirname});
 });
-app.get("/functions.js", (req, res) => {
-    res.sendFile("functions.js", {root: __dirname});
-});
-app.use("/scripts", express.static(__dirname + "/scripts"));
-app.use("/styles", express.static(__dirname + "/styles"));
-app.use("/public", express.static(__dirname + "/public"));
 
 
 // Reads a file from Cloudflare brainrot-panopto bucket
