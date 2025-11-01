e064c65aaefcc4bf28ccfee7332719bc917db591
sombat92 <44734452+sombat92@users.noreply.github.com>
2025-11-01 17:29:11 +0000

    Integrated html files

--- index.js
+++ index.js
@@ -2,7 +2,7 @@
     @aws-sdk/client-s3, cors, dotenv, express, express-session
 */
 
-const dotenv = require("dotenv");
+const dotenv = require("dotenv"); // Loads environment variables from .env
 dotenv.config();
 
 const cloudflare = require("@aws-sdk/client-s3");
@@ -24,15 +24,25 @@ const s3 = new cloudflare.S3Client({
     }
 });
 
-app.get("/", (req, res) => {
-    res.send("Hello world");
+app.get("/viewer", (req, res) => {
+    res.sendFile("viewer.html", {root: __dirname});
 });
 app.get("/test", (req, res) => {
     res.sendFile("test.html", {root: __dirname});
 });
+app.get("/login", (req, res) => {
+    res.sendFile("login.html", {root: __dirname});
+});
+app.get("/", (req, res) => {
+    res.sendFile("index.html", {root: __dirname});
+});
 app.get("/functions.js", (req, res) => {
     res.sendFile("functions.js", {root: __dirname});
 });
+app.use("/scripts", express.static(__dirname + "/scripts"));
+app.use("/styles", express.static(__dirname + "/styles"));
+app.use("/public", express.static(__dirname + "/public"));
+
 
 // Reads a file from Cloudflare brainrot-panopto bucket
 app.get("/read-file", async (req, res) => {
