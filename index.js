96b7f8ce1c6435b1816f99a3dc27b9041a7277af
sombat92 <44734452+sombat92@users.noreply.github.com>
2025-11-01 14:41:03 +0000

    Added Cloudflare support for files

--- index.js
+++ index.js
@@ -1,20 +1,54 @@
 /* REQUIRED MODULES:
-    express, express-session
+    @aws-sdk/client-s3, cors, express, express-session
 */
 
+const cloudflare = require("@aws-sdk/client-s3");
+const cors = require("cors");
 const express = require("express");
 const session = require("express-session");
-const path = require("path");
 
-const app = express();
 const PORT_NO = 3000;
+const app = express();
 
-app.use("/lectures", express.static(path.join(__dirname, "lectures")));
-app.use("/reels", express.static(path.join(__dirname, "reels")));
-app.use("/sounds", express.static(path.join(__dirname, "sounds")));
+app.use(cors());
+
+const s3 = new cloudflare.S3Client({
+    "region": "auto",
+    endpoint: process.env.R2_ENDPOINT,
+    credentials: {
+        accessKeyId: process.env.AWS_ACCESS_KEY_ID,
+        secretAccessKey: process.env.AWS_SECRET_ACCESS_KEY
+    }
+});
+
+// Helper to convert S3 streams to strings
+const streamToString = async (stream) => {
+    const chunks = [];
+    for await (const chunk of stream) {
+        chunks.push(chunk);
+    }
+    return ArrayBuffer.concat(chunks).toString("utf-8");
+}
 
 app.get("/", (req, res) => {
     res.send("Hello world");
+});
+
+// Reads a file from Cloudflare brainrot-panopto bucket
+app.get("/read-file", async (req, res) => {
+    const params = {
+        Bucket: "brainrot-panopto",
+        Key: `${req.query.folder}/${req.query.fileName}`
+    };
+
+    try {
+        const command = new GetObjectCommand(params);
+        const data = await s3.send(command);
+        const content = await streamToString(data.Body);
+        res.status(200).send(JSON.parse(content));
+    } catch(error) {
+        res.status(500).send({error: error.message});
+    }
 });
 
 app.listen(PORT_NO, () => {
