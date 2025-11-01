7f99487040e37accc8971fbd9a54b4b828cf8036
sombat92 <44734452+sombat92@users.noreply.github.com>
2025-11-01 14:06:49 +0000

    Remove Insta OAuth. Added lectures, reels and sounds folders for storage in server

--- index.js
+++ index.js
@@ -1,12 +1,22 @@
-//index.js
+/* REQUIRED MODULES:
+    express, express-session
+*/
 
 const express = require("express");
+const session = require("express-session");
+const path = require("path");
+
 const app = express();
+const PORT_NO = 3000;
 
-
-
+app.use("/lectures", express.static(path.join(__dirname, "lectures")));
+app.use("/reels", express.static(path.join(__dirname, "reels")));
+app.use("/sounds", express.static(path.join(__dirname, "sounds")));
 
 app.get("/", (req, res) => {
     res.send("Hello world");
 });
 
+app.listen(PORT_NO, () => {
+    console.log(`Server running on localhost:${PORT_NO}`);
+});
